2dfcc873629df9a01fcd54544315e3eb05dad47e
ripBlasterBR <ripblaster906@gmail.com>
2024-11-02 21:26:04 -0300

    Update redeem.js

--- api/redeem.js
+++ api/redeem.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     const { code } = req.query;
 
-    if (code === 'CÓDIGO1') {
+    if (code === 'UPDATE') {
         return res.status(200).json({ success: true, message: 'Código resgatado!', amount: 100 });
     } else {
         return res.status(404).json({ success: false, message: 'Código inválido.' });
