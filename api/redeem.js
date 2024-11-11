f62a63eabc52601c4a3bad00335a06c7f6361438
ripBlasterBR <ripblaster906@gmail.com>
2024-11-11 18:32:09 -0300

    Update redeem.js

--- api/redeem.js
+++ api/redeem.js
@@ -1,9 +1,7 @@
 export default function handler(req, res) {
     const { code } = req.query;
 
-    if (code === 'UPDATE') {
-        return res.status(200).json({ success: true, message: 'Código resgatado!', amount: 1000 });
-    } else if (code === 'MALDIÇÃO') {
+ if (code === 'NOVAERA') {
         return res.status(200).json({ success: true, message: 'Código resgatado!', amount: 500 });
     } else {
         return res.status(404).json({ success: false, message: 'Código inválido.' });
